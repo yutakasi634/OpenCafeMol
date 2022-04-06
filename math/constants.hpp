e96eb3af4c72a02fb8da4119d770952558f95bff
yutaka <p7dea87c7e4n7j@gmail.com>
2022-04-06 14:36:38 +0900

    feat: add file for constants

---
+++ math/constants.hpp
@@ -0,0 +1,12 @@
+#ifndef OPEN_AICG2_PLUS_MATH_CONSTANTS_HPP
+#define OPEN_AICG2_PLUS_MATH_CONSTANTS_HPP
+
+// define physical constnt
+static const double Na = 6.02214076e23;
+static const double kB = 1.380649e-23; // J/K
+static const double pi = 3.1415926535897932385;
+
+// prepare fundamental constant for calculation
+static const double cafetime = std::sqrt(1.0/OpenMM::KJPerKcal)*0.1; // Ps
+
+#endif // OPEN_AICG2PLUS_MATH_CONSTANTS_HPP
