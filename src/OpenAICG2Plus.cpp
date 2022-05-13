df0494ca699ff38abeac406b5c26cdc6fb976e86
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2022-05-13 12:02:24 +0900

    feat: finish implementation of FlexibleLocalAngle

--- src/OpenAICG2Plus.cpp
+++ src/OpenAICG2Plus.cpp
@@ -147,42 +147,46 @@ void simulateSH3(const std::string& input_file_name)
             {
                 std::cerr << "    BondAngle     : FlexibleLocalAngle" << std::endl;
 
-                for(const AAType aa_type : AAType())
+                for(const auto& [aa_type, spline_table] : fla_spline_table)
                 {
-                    const std::array<double, 10>& spline_table = fla_spline_table.at(aa_type);
-                    OpenMM::Continuous1DFunction spline_func =
-                        OpenMM::Continuous1DFunction(
+                    OpenMM::Continuous1DFunction* spline_func =
+                        new OpenMM::Continuous1DFunction(
                             std::vector<double>(spline_table.begin(), spline_table.end()),
                             fla_spline_min_theta, fla_spline_max_theta);
                     const double min_theta_y = spline_table[0];
                     const double max_theta_y = spline_table[9];
 
-                    OpenMM::CustomCompoundBondForce angle_ff =
-                        OpenMM::CustomCompoundBondForce(3,
+                    OpenMM::CustomCompoundBondForce* angle_ff =
+                        new OpenMM::CustomCompoundBondForce(3,
                             "k * ("
                                "spline(theta)"
                                "- (step(min_theta-theta) * 30 * (theta-min_theta) + min_theta_y)"
                                "+ (step(theta-max_theta) * 30 * (theta-max_theta) + max_theta_y)"
-                            ")");
-                    angle_ff.addTabulatedFunction("spline", &spline_func);
-                    angle_ff.addPerBondParameter("k");
-                    angle_ff.addPerBondParameter("min_theta");
-                    angle_ff.addPerBondParameter("min_theta_y");
-                    angle_ff.addPerBondParameter("max_theta");
-                    angle_ff.addPerBondParameter("max_theta_y");
+                            ");"
+                          "theta = angle(p1,p2,p3)");
+                    angle_ff->addTabulatedFunction("spline", spline_func);
+                    angle_ff->addPerBondParameter("k");
+                    angle_ff->addPerBondParameter("min_theta");
+                    angle_ff->addPerBondParameter("min_theta_y");
+                    angle_ff->addPerBondParameter("max_theta");
+                    angle_ff->addPerBondParameter("max_theta_y");
 
-                    const auto& params = toml::find<toml::array>(local_ff, "parameteres");
+                    const auto& params = toml::find<toml::array>(local_ff, "parameters");
                     for(const auto& param : params)
                     {
-                        const auto& indices =
-                            toml::find<std::vector<int>>(param, "indices");
-                        const double k =
-                            toml::find<double>(param, "k") * OpenMM::KJPerKcal; // KJ/mol
-                        angle_ff.addBond(indices,
-                                {k, fla_spline_min_theta, min_theta_y,
-                                 fla_spline_max_theta, max_theta_y});
+                        const std::string y = toml::find<std::string>(param, "y");
+                        if(true)//y.substr(3, 3) == aa_type) // y is like "y1_PHE"
+                        {
+                            const auto& indices =
+                                toml::find<std::vector<int>>(param, "indices");
+                            const double k =
+                                toml::find<double>(param, "k") * OpenMM::KJPerKcal; // KJ/mol
+                            angle_ff->addBond(indices,
+                                    {k, fla_spline_min_theta, min_theta_y,
+                                     fla_spline_max_theta, max_theta_y});
+                        }
                     }
-                    system.addForce(&angle_ff);
+                    system.addForce(angle_ff);
                 }
             }
             else if(interaction == "DihedralAngle" && potential == "Gaussian")
