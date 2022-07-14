7a6dffe0586e3303caccba54b1baf57ec065e521
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2022-07-14 15:46:37 +0900

    feat: make EXV use cutoff

--- src/forcefield/ExcludedVolumeForceFieldGenerator.hpp
+++ src/forcefield/ExcludedVolumeForceFieldGenerator.hpp
@@ -23,12 +23,15 @@ class ExcludedVolumeForceFieldGenerator
     {
         std::cerr << "    Global        : ExcludedVolume" << std::endl;
 
-        // TODO: add cutoff
         const std::string potential_formula = "epsilon*((sigma1+sigma2)/r)^12";
         auto exv_ff = std::make_unique<OpenMM::CustomNonbondedForce>(potential_formula);
+
+
         exv_ff->addPerParticleParameter("sigma");
         exv_ff->addGlobalParameter("epsilon", eps_);
 
+        double max_radius        = std::numeric_limits<double>::min();
+        double second_max_radius = std::numeric_limits<double>::min();
         const auto& itr = std::find(radiuses_.begin(), radiuses_.end(), std::nullopt);
         if(itr != radiuses_.end())
         {
@@ -40,6 +43,12 @@ class ExcludedVolumeForceFieldGenerator
                 {
                     exv_ff->addParticle({radius.value()});
                     participants.insert(idx);
+
+                    if(max_radius < radius.value())
+                    {
+                        max_radius        = radius.value();
+                        second_max_radius = max_radius;
+                    }
                 }
                 else
                 {
@@ -53,8 +62,17 @@ class ExcludedVolumeForceFieldGenerator
             for(const auto& radius : radiuses_)
             {
                 exv_ff->addParticle({radius.value()});
+                if(max_radius < radius.value())
+                 {
+                    max_radius        = radius.value();
+                    second_max_radius = max_radius;
+                 }
             }
         }
+
+        // set cutoff
+        exv_ff->setNonbondedMethod(OpenMM::CustomNonbondedForce::CutoffNonPeriodic);
+        exv_ff->setCutoffDistance((max_radius + second_max_radius)*2.0);
 
         for(const auto& exclusion_pair : exclusion_pairs_)
         {
