df12ee4476b66f441e90bd2f4bce6b5b433374a3
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2023-07-04 22:07:40 +0900

    refactor: remove add_exclusion function
    Some FFGenerator classes had an add_exclusion function, but those were not used anywhere. I'm sorry that was misleading for the reference implementation.

--- src/forcefield/CosineDihedralForceFieldGenerator.hpp
+++ src/forcefield/CosineDihedralForceFieldGenerator.hpp
@@ -74,14 +74,6 @@ class CosineDihedralForceFieldGenerator final : public ForceFieldGeneratorBase
         return torsion_ff;
     }
 
-    void add_exclusion(std::vector<index_pair_type>& exclusion_pairs) const noexcept
-    {
-        for(const auto& indices : indices_vec_)
-        {
-            exclusion_pairs.push_back(std::make_pair(indices[0], indices[3]));
-        }
-    }
-
     const std::vector<indices_type>& indices() const noexcept { return indices_vec_; }
     std::string name() const noexcept { return "CosineDihedral"; }
 
