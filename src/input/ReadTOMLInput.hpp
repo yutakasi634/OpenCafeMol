21a9f6f91ecc8779078dbc2eceb394f3ceac96ce
ToruNiina <niina.toru.68u@gmail.com>
2023-08-07 00:22:19 +0900

    feat: make optional platform_table an empty table
    if it is an empty value, toml::value::contains fails.

--- src/input/ReadTOMLInput.hpp
+++ src/input/ReadTOMLInput.hpp
@@ -768,7 +768,7 @@ Simulator read_toml_input(const std::string& toml_file_name)
     observers.push_back(std::make_unique<EnergyObserver>(output_path+output_prefix, system_gen));
 
     // read platform
-    const auto platform_table = toml::find_or(data, "platform", {});
+    const auto platform_table = toml::find_or(data, "platform", toml::value(toml::table{}));
     const auto platform_name  = toml::find_or<std::string>(platform_table, "name", std::string("CUDA"));
     std::cerr << "    OpenMM platform : " << platform_name << std::endl;
 
