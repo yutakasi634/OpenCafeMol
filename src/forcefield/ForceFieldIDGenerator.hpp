01530caa5e6e340021b37b205be44f11195cf162
ToruNiina <niina.toru.68u@gmail.com>
2023-08-07 00:32:51 +0900

    refactor: add comment to FFIDGen

--- src/forcefield/ForceFieldIDGenerator.hpp
+++ src/forcefield/ForceFieldIDGenerator.hpp
@@ -4,6 +4,13 @@
 #include <atomic>
 #include <cstddef>
 
+// OpenMM does not introduce namespace to parameters defined in CustomForces.
+// So we need to use Hungarian notation to parameters to avoid collisions.
+// Especially, since we define multiple FLA/FLD Forces, we need to add
+// forcefield-unique index to this notation, not only the forcefield name.
+// On some platform, OpenMM does not check parameter collision (CUDA does not,
+// but CPU does) before running simulation. It causes a fatal but silent bug.
+// To avoid this, we use ID generator that automatically generates unique idx.
 class ForceFieldIDGenerator
 {
   public:
