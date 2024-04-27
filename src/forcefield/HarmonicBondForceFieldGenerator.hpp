14edfbc9b7425f6bb8997e471858bd5e4bd1f9c0
ToruNiina <niina.toru.68u@gmail.com>
2024-04-27 15:30:31 +0900

    feat: split HarmonicBond

--- src/forcefield/HarmonicBondForceFieldGenerator.hpp
+++ src/forcefield/HarmonicBondForceFieldGenerator.hpp
@@ -40,18 +40,7 @@ class HarmonicBondForceFieldGenerator final : public ForceFieldGeneratorBase
         }
     }
 
-    std::unique_ptr<OpenMM::Force> generate() const noexcept override
-    {
-        auto bond_ff = std::make_unique<OpenMM::HarmonicBondForce>();
-        bond_ff->setUsesPeriodicBoundaryConditions(use_periodic_);
-        for(std::size_t idx=0; idx<indices_vec_.size(); ++idx)
-        {
-            const indices_type& idx_pair = indices_vec_[idx];
-            bond_ff->addBond(idx_pair.first, idx_pair.second, v0s_[idx], ks_[idx]);
-        }
-
-        return bond_ff;
-    }
+    std::unique_ptr<OpenMM::Force> generate() const noexcept override;
 
     const std::vector<indices_type>& indices() const noexcept { return indices_vec_; }
     std::string name() const noexcept override { return "HarmonicBond"; }
