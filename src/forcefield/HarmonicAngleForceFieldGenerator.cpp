11e6614045d8ed8dbb567c2c8bf716522e799a13
ToruNiina <niina.toru.68u@gmail.com>
2024-04-27 15:30:14 +0900

    feat: split HarmonicAngle

---
+++ src/forcefield/HarmonicAngleForceFieldGenerator.cpp
@@ -0,0 +1,15 @@
+#include "HarmonicAngleForceFieldGenerator.hpp"
+
+std::unique_ptr<OpenMM::Force> HarmonicAngleForceFieldGenerator::generate() const noexcept
+{
+    auto angle_ff = std::make_unique<OpenMM::HarmonicAngleForce>();
+    angle_ff->setUsesPeriodicBoundaryConditions(use_periodic_);
+    for(std::size_t idx=0; idx<indices_vec_.size(); ++idx)
+    {
+        const indices_type& idx_triplet = indices_vec_[idx];
+        angle_ff->addAngle(idx_triplet[0], idx_triplet[1], idx_triplet[2],
+                           v0s_[idx], ks_[idx]);
+    }
+
+    return angle_ff;
+}
