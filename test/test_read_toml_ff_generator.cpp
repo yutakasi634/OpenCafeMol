a3be3c79b8b20de831a2964a4cbdb44359a2be6e
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2025-01-30 12:31:43 +0900

    feat: add test and adjust cmake to toml11 v4.3.0

--- test/test_read_toml_ff_generator.cpp
+++ test/test_read_toml_ff_generator.cpp
@@ -1,9 +1,26 @@
 #include "src/input/ReadTOMLForceFieldGenerator.hpp"
 
+#include <toml.hpp>
+
 #include <gtest/gtest.h>
 
 
-TEST(sample, sample)
+TEST(ReadTOMLForceFieldGenerator, ReadTOMLHarmonicBondForceFieldGenerator)
 {
-    EXPECT_TRUE(true);
+    using namespace toml::literals;
+    const toml::value v = u8R"(
+        interaction = "BondLength"
+        potential   = "Harmonic"
+        topology    = "bond"
+        parameters  = [
+            {indices = [ 0, 1], v0 = 1.0, k = 2.0},
+            {indices = [ 3, 5], v0 = 3.0, k = 4.0},
+            {indices = [ 2, 6], v0 = 5.0, k = 6.0}
+        ]
+    )"_toml;
+
+    Topology topology(7);
+    bool     use_periodic = false;
+
+    EXPECT_NO_THROW(read_toml_harmonic_bond_ff_generator(v, topology, use_periodic));
 }
