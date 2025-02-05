9b64669b3ae4845e3a45f5eadff649c0b4929bde
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2025-02-05 16:05:57 +0900

    test: for read_top_file in ReadGenesisInput.cpp

--- test/test_read_genesis_input.cpp
+++ test/test_read_genesis_input.cpp
@@ -1,13 +1,9 @@
 #include "src/input/ReadGenesisInput.cpp"
 
 #include <gtest/gtest.h>
-#include <filesystem>
-#include <iostream>
 
 TEST(ReadGenesisInput, PreprocessTopFile)
 {
-    std::cout << "current path: " << std::filesystem::current_path() << std::endl;
-
     const std::vector<std::string> file_contents =
         preprocess_top_file("test_preprocess_top_file_input.top", "input/");
 
@@ -19,4 +15,14 @@ TEST(ReadGenesisInput, PreprocessTopFile)
               file_contents[2]);
     EXPECT_EQ("included_with_comment.inp was correctly included",
               file_contents[3]);
+}
+
+TEST(ReadGenesisInput, ReadTopFile)
+{
+    std::map<std::string, std::vector<std::string>> top_contents =
+        read_top_file("test_read_top_file_input.top", "input/");
+
+    EXPECT_EQ("first elem of first section", top_contents.at("first section")[0]);
+    EXPECT_EQ("second elem of second section", top_contents.at("second section")[1]);
+    EXPECT_EQ("third elem of third section", top_contents.at("third section")[2]);
 }
